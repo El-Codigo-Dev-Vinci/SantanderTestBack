422d038586654e42e318474eda8b7747a8f84fae
joaquinpettinari <joaquinpettinari@hotmail.com>
2021-04-21 23:04:46 -0300

    Changing User table data

--- db/migrations/20201211172124-create-usuario.js
+++ db/migrations/20201211172124-create-usuario.js
@@ -6,22 +6,39 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
-      nombre: {
-        type: Sequelize.STRING
+      name: {
+        type: Sequelize.STRING,
+      },
+      contrasenia: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: { msg: 'This email is already in use. Please use another' },
+        validate: {
+          isEmail: { msg: 'Invalid format' },
+        },
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.ENUM('invited', 'admin'),
+        defaultValue: 'invited',
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Usuarios');
-  }
+  },
 };
